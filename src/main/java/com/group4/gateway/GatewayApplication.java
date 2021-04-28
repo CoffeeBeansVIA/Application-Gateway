9bd4ed54cad4d982473b4a10b4fd4721f63257f6
Eugeniu Racila <eugeniu.racila@gmail.com>
2021-04-28 12:14:03 +0200

    Add threading

--- src/main/java/com/group4/gateway/GatewayApplication.java
+++ src/main/java/com/group4/gateway/GatewayApplication.java
@@ -10,5 +10,10 @@ public class GatewayApplication {
 //    }
     public static void main(String[] args) {
         WebsocketClient websocketClient = new WebsocketClient("wss://iotnet.cibicom.dk/app?token=vnoTwwAAABFpb3RuZXQuY2liaWNvbS5ka8Jer376b_vS6G62ZSL3pMU=");
+        websocketClient.sendDownLink("{cmd: 'tx', EUI: '0004A30B0021B92F', port: 1, confirmed: true, data: 'test text'}");
+
+        while (true) {
+
+        }
     }
 }
--- src/main/java/com/group4/gateway/WebsocketClient.java
+++ src/main/java/com/group4/gateway/WebsocketClient.java
@@ -6,11 +6,10 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.WebSocket;
 import java.nio.ByteBuffer;
-import java.util.concurrent.CompletionStage;
-import java.util.concurrent.CompletableFuture;
-import java.nio.charset.StandardCharsets;
+import java.util.concurrent.*;
 
 public class WebsocketClient implements WebSocket.Listener {
+    private final ScheduledExecutorService executorService;
     private WebSocket server = null;
     private final String URL = "wss://iotnet.cibicom.dk/app?token=vnoTwwAAABFpb3RuZXQuY2liaWNvbS5ka8Jer376b_vS6G62ZSL3pMU=";
     private final String TOKEN = "0004A30B0021B92F";
@@ -19,11 +18,13 @@ public class WebsocketClient implements WebSocket.Listener {
     // Must be in Json format according to https://github.com/ihavn/IoT_Semester_project/blob/master/LORA_NETWORK_SERVER.md
     public void sendDownLink(String jsonTelegram) {
         server.sendText(jsonTelegram,true);
+        System.out.println("sendDownLink executed");
     }
 
     // E.g. url: "wss://iotnet.cibicom.dk/app?token=vnoTwwAAABFpb3RuZXQuY2liaWNvbS5ka8Jer376b_vS6G62ZSL3pMU="
     // Substitute ????????????????? with the token you have been given - 0004A30B0021B92F
     public WebsocketClient(String url) {
+        executorService = Executors.newScheduledThreadPool(1);
         HttpClient client = HttpClient.newHttpClient();
         CompletableFuture<WebSocket> ws = client.newWebSocketBuilder()
                 .buildAsync(URI.create(url), this);
@@ -34,9 +35,16 @@ public class WebsocketClient implements WebSocket.Listener {
     public void onOpen(WebSocket webSocket) {
         // This WebSocket will invoke onText, onBinary, onPing, onPong or onClose methods on the associated listener (i.e. receive methods) up to n more times
         webSocket.request(1);
+
+        //keep connection alive
+        executorService.scheduleAtFixedRate(() -> {
+                    String data = "Ping";
+                    ByteBuffer payload = ByteBuffer.wrap(data.getBytes());
+                    server.sendPing(payload);
+                },
+                1, 1, TimeUnit.SECONDS);
+
         System.out.println("WebSocket Listener has been opened for requests.");
-        sendDownLink("{cmd: 'tx', EUI: '926F9B5931FCA94C', port: 1, confirmed: true, data: 'test text'}");
-        System.out.println("sendDownLink executed");
     }
 
     //onError()
